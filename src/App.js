0e644b74aa93a8570d0af2194778f76904d20584
amaca <amaca@umich.edu>
2020-09-04 03:02:13 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,49 @@
-import React from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+
+function Chippy(){
+  [memory, changeMemory] = useState(Uint8Array(4096).fill(0)); 
+  [data_registers, changeDataReg] = useState(Uint8Array(16).fill(0)); 
+  [address_resgiter, changeAddrReg] = useState(0); 
+  [stack, changeStack] = useState(Uint16Array(16).fill(0)); 
+  [stack_pointer, changeStackPointer] = useState(-1); 
+  [pc, changePC] = useState(0x200); 
+  [dt, changeDT] = useState(0); 
+  [st, changeST] = useState(0); 
+
+  function decode(instruction){
+
+  }
+
+  function OneCycle(){
+    //fetch the next instruction 
+    let next_inst = memory[pc];
+
+    //decode instruction 
+    let {opcode, args} = decode(next_inst); 
+
+    //execute instruction 
+    
+
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function App() {
   return (
